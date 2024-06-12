d7e41cad7370941fe12aad814427d3ae7fb9d4e3
Matias Ruben Gonzalez <matias.r.gozalez@gmail.com>
2024-06-12 00:48:50 -0300

    implementacion de guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductoListComponent } from './producto-list/producto-list.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './auth.guard';
+import { authGuard } from '../app/guards/auth.guard';
 import { CrearProductoComponent } from './crear-producto/crear-producto.component';
 import { ReporteComponent } from './reporte/reporte.component';
 
@@ -11,11 +11,10 @@ import { ReporteComponent } from './reporte/reporte.component';
 
 export const routes: Routes = [
     {path:'', component:LoginComponent},
-    {path:'products', component: ProductoListComponent, canActivate: [AuthGuard]},
-    { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'crear', component: CrearProductoComponent },
-      { path: 'reporte', component: ReporteComponent },
-      
+    {path:'products', component: ProductoListComponent, canActivate: [authGuard]},
+    {path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent },
+    {path: 'crear', component: CrearProductoComponent },
+    {path: 'reporte', component: ReporteComponent },
     {path:'**', redirectTo:'login', pathMatch: 'full'}
 ]
